d71beb4ae78e8dd08713901e4c3ac2fd543ed775
kartikadur <kartikadur@gmail.com>
2016-05-26 10:15:11 +0530

    Using perlin noise to generate procedural surface textures

---
+++ Procedural LandMass/Assets/Scripts/Noise.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public static class Noise {
+
+	public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset) {
+		float[,] noiseMap = new float[mapWidth, mapHeight];
+
+		System.Random pseudoRandom = new System.Random (seed);
+		Vector2[] octaveOffsets = new Vector2[octaves];
+		for(int i = 0; i < octaves; i++) {
+			float offsetX = pseudoRandom.Next (-100000, 100000) + offset.x;
+			float offsetY = pseudoRandom.Next (-100000, 100000) + offset.y;
+			octaveOffsets [i] = new Vector2 (offsetX, offsetY);
+		}
+
+		if (scale <= 0) {
+			scale = 0.0001f;
+		}
+
+		float maxNoiseHeight = float.MinValue;
+		float minNoiseHeight = float.MaxValue;
+
+		float halfMapWidth = mapWidth / 2;
+		float halfMapHeight = mapHeight / 2;
+
+		for (int y = 0; y < mapHeight; y++) {
+			for (int x = 0; x < mapWidth; x++) {
+
+				float amplitude = 1;
+				float frequency = 1;
+				float noiseHeight = 0;
+
+				for (int i = 0; i < octaves; i++) {
+					float sampleX = (x - halfMapWidth) / scale * frequency + octaveOffsets [i].x;
+					float sampleY = (y - halfMapHeight) / scale * frequency + octaveOffsets [i].y;
+
+					float perlinValue = Mathf.PerlinNoise (sampleX, sampleY) * 2 - 1;
+					noiseHeight += perlinValue * amplitude;
+
+					amplitude *= persistance;
+					frequency *= lacunarity;
+				}
+
+				if (noiseHeight > maxNoiseHeight) {
+					maxNoiseHeight = noiseHeight;
+				} else if (noiseHeight < minNoiseHeight) {
+					minNoiseHeight = noiseHeight;
+				}
+				noiseMap [x, y] = noiseHeight;
+			}
+		}
+
+		for (int y = 0; y < mapHeight; y++) {
+			for (int x = 0; x < mapWidth; x++) {
+				noiseMap [x, y] = Mathf.InverseLerp (minNoiseHeight, maxNoiseHeight, noiseMap [x, y]);
+			}
+		}
+
+		return noiseMap;
+	}
+
+}
